af37ceda0ecbd967518a390dd49cc6806134b2eb
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-02-01 21:36:39 +0000

    feat: Adding a button to change the color of that card when clicked and adding event listeners to those buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+// const myElement = document.getElementById("color-button");
+
+// myElement.addEventListener("click", function () {
+//   const posts = document.getElementsByClassName("card");
+//   console.log(posts);
+//   [...posts].forEach(
+//     (post) =>
+//       (post.style.background = ` rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`)
+//   );
+// });
+
+const posts = document.getElementsByClassName("card");
+
+function resetCardsColor(){
+  [...posts].forEach((card, indx) => {
+    const buttonId = `color-button_${indx}`;
+    // console.log(buttonId);
+    const colorButton = document.getElementById(buttonId);
+    card.style.background ="#151820";
+  })
+}
+
+[...posts].forEach((card, indx) => {
+  const buttonId = `color-button_${indx}`;
+  // console.log(buttonId);
+  const colorButton = document.getElementById(buttonId);
+  colorButton.addEventListener("click", (e) => {
+    resetCardsColor();
+    card.style.background = ` rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+  });
 });
